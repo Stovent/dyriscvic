067714cda1bcb4d0c8f4e94da246e505d0e267ab
Unknown <elian.hamon@viacesi.fr>
2021-05-05 22:32:34 +0200

    more performances

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -17,65 +17,65 @@ pub fn get_instruction_length(inst: u16) -> u16 {
 }
 
 #[derive(Clone, Copy, Debug)]
-pub struct Instruction {
-    pub pc: u32,
-    pub rd: usize,
-    pub rs1: usize,
-    pub rs2: usize,
+pub struct Instruction<T> {
+    pub pc: T,
+    pub rd: u8,
+    pub rs1: u8,
+    pub rs2: u8,
     pub imm: i32,
 }
 
-impl Instruction {
-    pub fn new_empty(pc: u32) -> Self {
+impl<T> Instruction<T> {
+    pub fn new_empty(pc: T) -> Self {
         Self { pc, rd: 0, rs1: 0, rs2: 0, imm: 0 }
     }
-}
 
-pub fn decode_type_fail(pc: u32, opcode: u32) -> Instruction {
-    println!("Bad format {:#X}", opcode);
-    Instruction::new_empty(pc)
-}
+    pub fn decode_type_fail(pc: T, opcode: u32) -> Instruction<T> {
+        println!("Bad format {:#X}", opcode);
+        Instruction::new_empty(pc)
+    }
 
-pub fn decode_type_empty(pc: u32, _: u32) -> Instruction {
-    Instruction::new_empty(pc)
-}
+    pub fn decode_type_empty(pc: T, _: u32) -> Instruction<T> {
+        Instruction::new_empty(pc)
+    }
 
-pub fn decode_type_r(pc: u32, opcode: u32) -> Instruction {
-    let rd = (opcode >> 7 & 0b1_1111) as usize;
-    let rs1 = (opcode >> 15 & 0b1_1111) as usize;
-    let rs2 = (opcode >> 20 & 0b1_1111) as usize;
-    Instruction { pc, rd, rs1, rs2, imm: 0 }
-}
+    pub fn decode_type_r(pc: T, opcode: u32) -> Instruction<T> {
+        let rd = (opcode >> 7 & 0b1_1111) as u8;
+        let rs1 = (opcode >> 15 & 0b1_1111) as u8;
+        let rs2 = (opcode >> 20 & 0b1_1111) as u8;
+        Instruction { pc, rd, rs1, rs2, imm: 0 }
+    }
 
-pub fn decode_type_i(pc: u32, opcode: u32) -> Instruction {
-    let rd = (opcode >> 7 & 0b1_1111) as usize;
-    let rs1 = (opcode >> 15 & 0b1_1111) as usize;
-    let imm = opcode as i32 >> 20;
-    Instruction { pc, rd, rs1, rs2: 0, imm }
-}
+    pub fn decode_type_i(pc: T, opcode: u32) -> Instruction<T> {
+        let rd = (opcode >> 7 & 0b1_1111) as u8;
+        let rs1 = (opcode >> 15 & 0b1_1111) as u8;
+        let imm = opcode as i32 >> 20;
+        Instruction { pc, rd, rs1, rs2: 0, imm }
+    }
 
-pub fn decode_type_s(pc: u32, opcode: u32) -> Instruction {
-    let rs1 = (opcode >> 15 & 0b1_1111) as usize;
-    let rs2 = (opcode >> 20 & 0b1_1111) as usize;
-    let imm = opcode as i32 >> 20 & 0xFFFF_FFE0 | opcode as i32 >> 7 & 0b1_1111;
-    Instruction { pc, rd: 0, rs1, rs2, imm}
-}
+    pub fn decode_type_s(pc: T, opcode: u32) -> Instruction<T> {
+        let rs1 = (opcode >> 15 & 0b1_1111) as u8;
+        let rs2 = (opcode >> 20 & 0b1_1111) as u8;
+        let imm = opcode as i32 >> 20 & 0xFFFF_FFE0 | opcode as i32 >> 7 & 0b1_1111;
+        Instruction { pc, rd: 0, rs1, rs2, imm}
+    }
 
-pub fn decode_type_b(pc: u32, opcode: u32) -> Instruction {
-    let rs1 = (opcode >> 15 & 0b1_1111) as usize;
-    let rs2 = (opcode >> 20 & 0b1_1111) as usize;
-    let imm = opcode as i32 >> 19 & 0x1000 | (opcode as i32) << 4 & 0x0800 | opcode as i32 >> 20 & 0x07E0 | opcode as i32 >> 7 & 0x001E;
-    Instruction { pc, rd: 0, rs1, rs2, imm}
-}
+    pub fn decode_type_b(pc: T, opcode: u32) -> Instruction<T> {
+        let rs1 = (opcode >> 15 & 0b1_1111) as u8;
+        let rs2 = (opcode >> 20 & 0b1_1111) as u8;
+        let imm = opcode as i32 >> 19 & 0x1000 | (opcode as i32) << 4 & 0x0800 | opcode as i32 >> 20 & 0x07E0 | opcode as i32 >> 7 & 0x001E;
+        Instruction { pc, rd: 0, rs1, rs2, imm}
+    }
 
-pub fn decode_type_u(pc: u32, opcode: u32) -> Instruction {
-    let rd = (opcode >> 7 & 0b1_1111) as usize;
-    let imm = opcode as i32 & 0xFFFF_F000;
-    Instruction { pc, rd, rs1: 0, rs2: 0, imm }
-}
+    pub fn decode_type_u(pc: T, opcode: u32) -> Instruction<T> {
+        let rd = (opcode >> 7 & 0b1_1111) as u8;
+        let imm = opcode as i32 & 0xFFFF_F000;
+        Instruction { pc, rd, rs1: 0, rs2: 0, imm }
+    }
 
-pub fn decode_type_j(pc: u32, opcode: u32) -> Instruction {
-    let rd = (opcode >> 7 & 0b1_1111) as usize;
-    let imm = opcode as i32 >> 11 & 0xFFF0_0000 | opcode as i32 & 0xF_F000 | opcode as i32 >> 9 & 0x0800 | opcode as i32 >> 20 & 0x7FE;
-    Instruction { pc, rd, rs1: 0, rs2: 0, imm}
+    pub fn decode_type_j(pc: T, opcode: u32) -> Instruction<T> {
+        let rd = (opcode >> 7 & 0b1_1111) as u8;
+        let imm = opcode as i32 >> 11 & 0xFFF0_0000 | opcode as i32 & 0xF_F000 | opcode as i32 >> 9 & 0x0800 | opcode as i32 >> 20 & 0x7FE;
+        Instruction { pc, rd, rs1: 0, rs2: 0, imm}
+    }
 }
--- src/rv32i/rv32i.rs
+++ src/rv32i/rv32i.rs
@@ -5,7 +5,7 @@ pub struct RV32I<'a> {
     pub x: [i32; 32],
     pub pc: u32,
 
-    pub inst: Instruction,
+    pub inst: Instruction<u32>,
     pub ext: String,
     pub eei: &'a mut dyn ExecutionEnvironmentInterface,
 }
@@ -58,9 +58,9 @@ enum ISA {
 impl<'a> RV32I<'a> {
     pub fn single_step(&mut self) {
         let pc = self.pc;
+        self.pc += 4;
         let opcode = self.eei.get32le(pc as u64);
         let inst_size = get_instruction_length(opcode as u16);
-        self.pc += 4;
         match inst_size {
 //            2 => if self.ext.contains('C'),
             4 => {
@@ -72,19 +72,19 @@ impl<'a> RV32I<'a> {
 
                 RV32I::EXECUTE[inst as usize](self);
             },
-            _ => println!("Unknown opcode {:#X} at {:#x}", opcode, self.pc - 4),
+            _ => println!("Unknown opcode {:#X} at {:#x}", opcode, pc),
         };
     }
 }
 
 // Decoder
 impl<'a> RV32I<'a> {
-    const FORMAT: [fn(u32, u32) -> Instruction; 41] = [
-        decode_type_fail, decode_type_r, decode_type_i, decode_type_r, decode_type_i,     decode_type_u,     decode_type_b, decode_type_b,
-        decode_type_b,    decode_type_b, decode_type_b, decode_type_b, decode_type_empty, decode_type_empty, decode_type_i, decode_type_j,
-        decode_type_i,    decode_type_i, decode_type_i, decode_type_i, decode_type_i,     decode_type_u,     decode_type_i, decode_type_r,
-        decode_type_i,    decode_type_s, decode_type_s, decode_type_r, decode_type_r,     decode_type_r,     decode_type_i, decode_type_i,
-        decode_type_r,    decode_type_r, decode_type_r, decode_type_r, decode_type_r,     decode_type_r,     decode_type_s, decode_type_r, decode_type_i,
+    const FORMAT: [fn(u32, u32) -> Instruction<u32>; 41] = [
+        Instruction::decode_type_fail, Instruction::decode_type_r, Instruction::decode_type_i, Instruction::decode_type_r, Instruction::decode_type_i, Instruction::decode_type_u, Instruction::decode_type_b, Instruction::decode_type_b,
+        Instruction::decode_type_b, Instruction::decode_type_b, Instruction::decode_type_b, Instruction::decode_type_b, Instruction::decode_type_empty, Instruction::decode_type_empty, Instruction::decode_type_i, Instruction::decode_type_j,
+        Instruction::decode_type_i, Instruction::decode_type_i, Instruction::decode_type_i, Instruction::decode_type_i, Instruction::decode_type_i, Instruction::decode_type_u, Instruction::decode_type_i, Instruction::decode_type_r,
+        Instruction::decode_type_i, Instruction::decode_type_s, Instruction::decode_type_s, Instruction::decode_type_r, Instruction::decode_type_r, Instruction::decode_type_r, Instruction::decode_type_i, Instruction::decode_type_i,
+        Instruction::decode_type_r, Instruction::decode_type_r, Instruction::decode_type_r, Instruction::decode_type_r, Instruction::decode_type_r, Instruction::decode_type_r, Instruction::decode_type_s, Instruction::decode_type_r, Instruction::decode_type_i,
     ];
 
     const ISA_ARITHMETIC: [ISA; 8] = [ISA::ADD, ISA::SLL, ISA::SLT, ISA::SLTU, ISA::XOR, ISA::SRL, ISA::OR, ISA::AND];
@@ -144,23 +144,23 @@ impl<'a> RV32I<'a> {
     pub fn UNKNOWN(&mut self) {}
 
     pub fn ADD(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] + self.x[self.inst.rs2];
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] + self.x[self.inst.rs2 as usize];
     }
 
     pub fn ADDI(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] + self.inst.imm;
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] + self.inst.imm;
     }
 
     pub fn AND(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] & self.x[self.inst.rs2];
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] & self.x[self.inst.rs2 as usize];
     }
 
     pub fn ANDI(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] & self.inst.imm;
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] & self.inst.imm;
     }
 
     pub fn AUIPC(&mut self) {
-        self.x[self.inst.rd] = self.inst.pc as i32 + self.inst.imm;
+        self.x[self.inst.rd as usize] = self.inst.pc as i32 + self.inst.imm;
     }
 
     pub fn BEQ(&mut self) {
@@ -191,7 +191,7 @@ impl<'a> RV32I<'a> {
     }
 
     pub fn JAL(&mut self) {
-        self.x[self.inst.rd] = self.inst.pc as i32 + 4;
+        self.x[self.inst.rd as usize] = self.inst.pc as i32 + 4;
         self.pc = (self.inst.pc as i32 + self.inst.imm) as u32;
     }
 
@@ -200,7 +200,7 @@ impl<'a> RV32I<'a> {
 
     pub fn LB(&mut self) {
         // if rd == 0, throw exception
-        self.x[self.inst.rd] = self.eei.get8((self.x[self.inst.rs1] + self.inst.imm) as u64) as i8 as i32;
+        self.x[self.inst.rd as usize] = self.eei.get8((self.x[self.inst.rs1 as usize] + self.inst.imm) as u64) as i8 as i32;
     }
 
     pub fn LBU(&mut self) {
@@ -213,18 +213,18 @@ impl<'a> RV32I<'a> {
     }
 
     pub fn LUI(&mut self) {
-        self.x[self.inst.rd] = self.inst.imm;
+        self.x[self.inst.rd as usize] = self.inst.imm;
     }
 
     pub fn LW(&mut self) {
     }
 
     pub fn OR(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] | self.x[self.inst.rs2];
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] | self.x[self.inst.rs2 as usize];
     }
 
     pub fn ORI(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] | self.inst.imm;
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] | self.inst.imm;
     }
 
     pub fn SB(&mut self) {
@@ -234,64 +234,64 @@ impl<'a> RV32I<'a> {
     }
 
     pub fn SLL(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] << (self.x[self.inst.rs2] & 0x1F);
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] << (self.x[self.inst.rs2 as usize] & 0x1F);
     }
 
     pub fn SLLI(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] << (self.inst.imm & 0x1F);
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] << (self.inst.imm & 0x1F);
     }
 
     pub fn SLT(&mut self) {
-        self.x[self.inst.rd] = (self.x[self.inst.rs1] < self.x[self.inst.rs2]) as i32;
+        self.x[self.inst.rd as usize] = (self.x[self.inst.rs1 as usize] < self.x[self.inst.rs2 as usize]) as i32;
     }
 
     pub fn SLTI(&mut self) {
-        self.x[self.inst.rd] = (self.x[self.inst.rs1] < self.inst.imm) as i32;
+        self.x[self.inst.rd as usize] = (self.x[self.inst.rs1 as usize] < self.inst.imm) as i32;
     }
 
     pub fn SLTIU(&mut self) {
-        self.x[self.inst.rd] = ((self.x[self.inst.rs1] as u32) < (self.inst.imm as u32)) as i32;
+        self.x[self.inst.rd as usize] = ((self.x[self.inst.rs1 as usize] as u32) < (self.inst.imm as u32)) as i32;
     }
 
     pub fn SLTU(&mut self) {
-        self.x[self.inst.rd] = ((self.x[self.inst.rs1] as u32) < self.x[self.inst.rs2] as u32) as i32;
+        self.x[self.inst.rd as usize] = ((self.x[self.inst.rs1 as usize] as u32) < self.x[self.inst.rs2 as usize] as u32) as i32;
     }
 
     pub fn SRA(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] >> (self.x[self.inst.rs2] & 0x1F);
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] >> (self.x[self.inst.rs2 as usize] & 0x1F);
     }
 
     pub fn SRAI(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] >> (self.inst.imm & 0x1F);
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] >> (self.inst.imm & 0x1F);
     }
 
     pub fn SRL(&mut self) {
-        self.x[self.inst.rd] = ((self.x[self.inst.rs1] as u32) >> (self.x[self.inst.rs2] as u32 & 0x1F)) as i32;
+        self.x[self.inst.rd as usize] = ((self.x[self.inst.rs1 as usize] as u32) >> (self.x[self.inst.rs2 as usize] as u32 & 0x1F)) as i32;
     }
 
     pub fn SRLI(&mut self) {
-        self.x[self.inst.rd] = (self.x[self.inst.rs1] as u32 >> (self.inst.imm as u32 & 0x1F)) as i32;
+        self.x[self.inst.rd as usize] = (self.x[self.inst.rs1 as usize] as u32 >> (self.inst.imm as u32 & 0x1F)) as i32;
     }
 
     pub fn SUB(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] - self.x[self.inst.rs2];
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] - self.x[self.inst.rs2 as usize];
     }
 
     pub fn SW(&mut self) {
     }
 
     pub fn XOR(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] ^ self.x[self.inst.rs2];
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] ^ self.x[self.inst.rs2 as usize];
     }
 
     pub fn XORI(&mut self) {
-        self.x[self.inst.rd] = self.x[self.inst.rs1] ^ self.inst.imm;
+        self.x[self.inst.rd as usize] = self.x[self.inst.rs1 as usize] ^ self.inst.imm;
     }
 }
 
 // Disassembler
 impl<'a> RV32I<'a> {
-    const DISASSEMBLE: [fn(Instruction); 41] = [
+    const DISASSEMBLE: [fn(Instruction<u32>); 41] = [
         RV32I::disassemble_UNKNOWN, RV32I::disassemble_ADD, RV32I::disassemble_ADDI, RV32I::disassemble_AND,  RV32I::disassemble_ANDI,   RV32I::disassemble_AUIPC, RV32I::disassemble_BEQ,   RV32I::disassemble_BGE,
         RV32I::disassemble_BGEU,    RV32I::disassemble_BLT, RV32I::disassemble_BLTU, RV32I::disassemble_BNE,  RV32I::disassemble_EBREAK, RV32I::disassemble_ECALL, RV32I::disassemble_FENCE, RV32I::disassemble_JAL,
         RV32I::disassemble_JALR,    RV32I::disassemble_LB,  RV32I::disassemble_LBU,  RV32I::disassemble_LH,   RV32I::disassemble_LHU,    RV32I::disassemble_LUI,   RV32I::disassemble_LW,    RV32I::disassemble_OR,
@@ -299,167 +299,167 @@ impl<'a> RV32I<'a> {
         RV32I::disassemble_SLTU,    RV32I::disassemble_SRA, RV32I::disassemble_SRAI, RV32I::disassemble_SRL,  RV32I::disassemble_SRLI,   RV32I::disassemble_SUB,   RV32I::disassemble_SW,    RV32I::disassemble_XOR,   RV32I::disassemble_XORI,
     ];
 
-    pub fn disassemble_UNKNOWN(inst: Instruction) {
+    pub fn disassemble_UNKNOWN(inst: Instruction<u32>) {
         println!("Error: unknown instruction {:?}", inst);
     }
 
-    pub fn disassemble_ADD(inst: Instruction) {
+    pub fn disassemble_ADD(inst: Instruction<u32>) {
         println!("Instruction: ADD {:?}", inst);
     }
 
-    pub fn disassemble_ADDI(inst: Instruction) {
+    pub fn disassemble_ADDI(inst: Instruction<u32>) {
         println!("Instruction: ADDI {:?}", inst);
     }
 
-    pub fn disassemble_AND(inst: Instruction) {
+    pub fn disassemble_AND(inst: Instruction<u32>) {
         println!("Instruction: AND {:?}", inst);
     }
 
-    pub fn disassemble_ANDI(inst: Instruction) {
+    pub fn disassemble_ANDI(inst: Instruction<u32>) {
         println!("Instruction: ANDI {:?}", inst);
     }
 
-    pub fn disassemble_AUIPC(inst: Instruction) {
+    pub fn disassemble_AUIPC(inst: Instruction<u32>) {
         println!("Instruction: AUIPC {:?}", inst);
     }
 
-    pub fn disassemble_BEQ(inst: Instruction) {
+    pub fn disassemble_BEQ(inst: Instruction<u32>) {
         println!("Instruction: BEQ {:?}", inst);
     }
 
-    pub fn disassemble_BGE(inst: Instruction) {
+    pub fn disassemble_BGE(inst: Instruction<u32>) {
         println!("Instruction: BGE {:?}", inst);
     }
 
-    pub fn disassemble_BGEU(inst: Instruction) {
+    pub fn disassemble_BGEU(inst: Instruction<u32>) {
         println!("Instruction: BGEU {:?}", inst);
     }
 
-    pub fn disassemble_BLT(inst: Instruction) {
+    pub fn disassemble_BLT(inst: Instruction<u32>) {
         println!("Instruction: BLT {:?}", inst);
     }
 
-    pub fn disassemble_BLTU(inst: Instruction) {
+    pub fn disassemble_BLTU(inst: Instruction<u32>) {
         println!("Instruction: BLTU {:?}", inst);
     }
 
-    pub fn disassemble_BNE(inst: Instruction) {
+    pub fn disassemble_BNE(inst: Instruction<u32>) {
         println!("Instruction: BNE {:?}", inst);
     }
 
-    pub fn disassemble_EBREAK(inst: Instruction) {
+    pub fn disassemble_EBREAK(inst: Instruction<u32>) {
         println!("Instruction: EBREAK {:?}", inst);
     }
 
-    pub fn disassemble_ECALL(inst: Instruction) {
+    pub fn disassemble_ECALL(inst: Instruction<u32>) {
         println!("Instruction: ECALL {:?}", inst);
     }
 
-    pub fn disassemble_FENCE(inst: Instruction) {
+    pub fn disassemble_FENCE(inst: Instruction<u32>) {
         println!("Instruction: FENCE {:?}", inst);
     }
 
-    pub fn disassemble_JAL(inst: Instruction) {
+    pub fn disassemble_JAL(inst: Instruction<u32>) {
         println!("Instruction: JAL {:?}", inst);
     }
 
-    pub fn disassemble_JALR(inst: Instruction) {
+    pub fn disassemble_JALR(inst: Instruction<u32>) {
         println!("Instruction: JALR {:?}", inst);
     }
 
-    pub fn disassemble_LB(inst: Instruction) {
+    pub fn disassemble_LB(inst: Instruction<u32>) {
         println!("Instruction: LB {:?}", inst);
     }
 
-    pub fn disassemble_LBU(inst: Instruction) {
+    pub fn disassemble_LBU(inst: Instruction<u32>) {
         println!("Instruction: LBU {:?}", inst);
     }
 
-    pub fn disassemble_LH(inst: Instruction) {
+    pub fn disassemble_LH(inst: Instruction<u32>) {
         println!("Instruction: LH {:?}", inst);
     }
 
-    pub fn disassemble_LHU(inst: Instruction) {
+    pub fn disassemble_LHU(inst: Instruction<u32>) {
         println!("Instruction: LHU {:?}", inst);
     }
 
-    pub fn disassemble_LUI(inst: Instruction) {
+    pub fn disassemble_LUI(inst: Instruction<u32>) {
         println!("Instruction: LUI {:?}", inst);
     }
 
-    pub fn disassemble_LW(inst: Instruction) {
+    pub fn disassemble_LW(inst: Instruction<u32>) {
         println!("Instruction: LW {:?}", inst);
     }
 
-    pub fn disassemble_OR(inst: Instruction) {
+    pub fn disassemble_OR(inst: Instruction<u32>) {
         println!("Instruction: OR {:?}", inst);
     }
 
-    pub fn disassemble_ORI(inst: Instruction) {
+    pub fn disassemble_ORI(inst: Instruction<u32>) {
         println!("Instruction: ORI {:?}", inst);
     }
 
-    pub fn disassemble_SB(inst: Instruction) {
+    pub fn disassemble_SB(inst: Instruction<u32>) {
         println!("Instruction: SB {:?}", inst);
     }
 
-    pub fn disassemble_SH(inst: Instruction) {
+    pub fn disassemble_SH(inst: Instruction<u32>) {
         println!("Instruction: SH {:?}", inst);
     }
 
-    pub fn disassemble_SLL(inst: Instruction) {
+    pub fn disassemble_SLL(inst: Instruction<u32>) {
         println!("Instruction: SLL {:?}", inst);
     }
 
-    pub fn disassemble_SLLI(inst: Instruction) {
+    pub fn disassemble_SLLI(inst: Instruction<u32>) {
         println!("Instruction: SLLI {:?}", inst);
     }
 
-    pub fn disassemble_SLT(inst: Instruction) {
+    pub fn disassemble_SLT(inst: Instruction<u32>) {
         println!("Instruction: SLT {:?}", inst);
     }
 
-    pub fn disassemble_SLTI(inst: Instruction) {
+    pub fn disassemble_SLTI(inst: Instruction<u32>) {
         println!("Instruction: SLTI {:?}", inst);
     }
 
-    pub fn disassemble_SLTIU(inst: Instruction) {
+    pub fn disassemble_SLTIU(inst: Instruction<u32>) {
         println!("Instruction: SLTIU {:?}", inst);
     }
 
-    pub fn disassemble_SLTU(inst: Instruction) {
+    pub fn disassemble_SLTU(inst: Instruction<u32>) {
         println!("Instruction: SLTU {:?}", inst);
     }
 
-    pub fn disassemble_SRA(inst: Instruction) {
+    pub fn disassemble_SRA(inst: Instruction<u32>) {
         println!("Instruction: SRA {:?}", inst);
     }
 
-    pub fn disassemble_SRAI(inst: Instruction) {
+    pub fn disassemble_SRAI(inst: Instruction<u32>) {
         println!("Instruction: SRAI {:?}", inst);
     }
 
-    pub fn disassemble_SRL(inst: Instruction) {
+    pub fn disassemble_SRL(inst: Instruction<u32>) {
         println!("Instruction: SRL {:?}", inst);
     }
 
-    pub fn disassemble_SRLI(inst: Instruction) {
+    pub fn disassemble_SRLI(inst: Instruction<u32>) {
         println!("Instruction: SRLI {:?}", inst);
     }
 
-    pub fn disassemble_SUB(inst: Instruction) {
+    pub fn disassemble_SUB(inst: Instruction<u32>) {
         println!("Instruction: SUB {:?}", inst);
     }
 
-    pub fn disassemble_SW(inst: Instruction) {
+    pub fn disassemble_SW(inst: Instruction<u32>) {
         println!("Instruction: SW {:?}", inst);
     }
 
-    pub fn disassemble_XOR(inst: Instruction) {
+    pub fn disassemble_XOR(inst: Instruction<u32>) {
         println!("Instruction: XOR {:?}", inst);
     }
 
-    pub fn disassemble_XORI(inst: Instruction) {
+    pub fn disassemble_XORI(inst: Instruction<u32>) {
         println!("Instruction: XORI {:?}", inst);
     }
 }
